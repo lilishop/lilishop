cf03a0dec517150e2750034e93f8b0e7b86ad8b9
Chopper <liushuai711@gmail.com>
2021-05-13 15:14:41 +0800

    正确引入问题处理

--- socket-api/src/main/java/cn/lili/scocket/listener/MessageSendListener.java
+++ socket-api/src/main/java/cn/lili/scocket/listener/MessageSendListener.java
@@ -7,7 +7,6 @@ import cn.lili.modules.message.entity.dos.Message;
 import cn.lili.modules.message.entity.enums.MessageShowType;
 import cn.lili.modules.message.entity.vos.MessageShowVO;
 import cn.lili.modules.message.mapper.StoreMessageMapper;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
@@ -24,11 +23,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @Component
 @CrossOrigin
 @Slf4j
-
 @RocketMQMessageListener(topic = "${lili.data.rocketmq.other-topic}", consumerGroup = "${lili.data.rocketmq.other-group}")
 public class MessageSendListener implements RocketMQListener<MessageExt> {
 
-    private final StoreMessageMapper storeMessageMapper;
+    @Autowired
+    private StoreMessageMapper storeMessageMapper;
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
