903cf0b28a592e37dc0a506a2280d662ea2a2570
Chopper <liushuai711@gmail.com>
2021-05-24 15:32:20 +0800

    输出脚本进程

--- pushGithub.sh
+++ pushGithub.sh
@@ -1,15 +1,17 @@
+echo '开始推送github'
+echo '切换git地址'
 git remote rm origin
 
 git remote add origin git@github.com:hongyehuicheng/lilishop.git
-
+echo '设置上传代码分支，推送github'
 git push --set-upstream origin master
-
+echo '推送github完成'
 git remote rm origin
 
 git remote add origin git@gitee.com:beijing_hongye_huicheng/lilishop.git
-
-sleep 1
-
+echo '切回gitee资源'
 git branch --set-upstream-to=origin/master master
-
+echo '设置git跟踪资源'
+sleep 1
+echo '更新代码'
 git pull
