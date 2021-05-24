f12d0d1be05dc9ec11caeceffa093e61c3ed4d5d
Chopper <liushuai711@gmail.com>
2021-05-24 15:28:00 +0800

    调整推送脚本

--- pushGithub.sh
+++ pushGithub.sh
@@ -1,7 +1,15 @@
 git remote rm origin
+
 git remote add origin git@github.com:hongyehuicheng/lilishop.git
+
 git push --set-upstream origin master
+
 git remote rm origin
+
 git remote add origin git@gitee.com:beijing_hongye_huicheng/lilishop.git
+
+sleep 1
+
 git branch --set-upstream-to=origin/master master
+
 git pull
