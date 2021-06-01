b2d149c71483b4e5efeaabab1ad3111266a2245c
lifenlong <pikachu1995@126.com>
2021-06-01 22:10:28 +0800

    支持使用促销类型进行筛选订单：拼团、积分、赠品

--- framework/src/main/java/cn/lili/modules/order/order/entity/dto/OrderSearchParams.java
+++ framework/src/main/java/cn/lili/modules/order/order/entity/dto/OrderSearchParams.java
@@ -67,8 +67,9 @@ public class OrderSearchParams extends PageVO {
 
     /**
      * @see OrderTypeEnum
+     * @see cn.lili.modules.order.order.entity.enums.OrderPromotionTypeEnum
      */
-    @ApiModelProperty(value = "订单类型")
+    @ApiModelProperty(value = "订单类型",allowableValues = "NORMAL,VIRTUAL,GIFT,PINTUAN,POINT" )
     private String orderType;
 
     @ApiModelProperty(value = "支付方式")
@@ -136,7 +137,8 @@ public class OrderSearchParams extends PageVO {
 
         // 按订单类型
         if (StringUtils.isNotEmpty(orderType)) {
-            wrapper.eq("o.order_type", orderType);
+            wrapper.eq("o.order_type", orderType)
+                    .or().eq("o.order_promotion_type", orderType);
         }
 
         // 按支付方式
