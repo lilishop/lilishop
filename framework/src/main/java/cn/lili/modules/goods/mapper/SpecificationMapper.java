94bb98bf1ff0a86e7ee9eb70bcc0362820813627
Chopper <liushuai711@gmail.com>
2021-06-03 15:35:20 +0800

    去除Mapper 采用注解来实现相关查询

--- framework/src/main/java/cn/lili/modules/goods/mapper/SpecificationMapper.java
+++ framework/src/main/java/cn/lili/modules/goods/mapper/SpecificationMapper.java
@@ -3,10 +3,13 @@ package cn.lili.modules.goods.mapper;
 
 import cn.lili.modules.goods.entity.dos.Specification;
 import cn.lili.modules.goods.entity.vos.SpecificationVO;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * 规格数据处理层
@@ -18,8 +21,9 @@ public interface SpecificationMapper extends BaseMapper<Specification> {
 
     /**
      * 查询规格信息列表
-     * @param param
      * @return
      */
-    List<SpecificationVO> findSpecList(Map<String,Object> param);
+    @Select("SELECT s.id, s.spec_name,s.create_time ,GROUP_CONCAT(sv.spec_value SEPARATOR ',') AS spec_value" +
+            " FROM li_specification s LEFT JOIN li_spec_values sv ON s.id = sv.spec_id ${ew.customSqlSegment} ")
+    List<SpecificationVO> findSpecList(@Param(Constants.WRAPPER) Wrapper queryWrapper);
 }
