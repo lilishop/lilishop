f11c3205ee62752c8c4e159ecccec6d5f0f47a86
Chopper <liushuai711@gmail.com>
2021-06-04 10:21:54 +0800

    攻击sql阻断解析器

--- framework/src/main/java/cn/lili/base/mybatisplus/MybatisPlusConfig.java
+++ framework/src/main/java/cn/lili/base/mybatisplus/MybatisPlusConfig.java
@@ -1,9 +1,14 @@
 package cn.lili.base.mybatisplus;
 
+import com.baomidou.mybatisplus.core.parser.ISqlParser;
+import com.baomidou.mybatisplus.extension.parsers.BlockAttackSqlParser;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Chopper
@@ -16,6 +21,12 @@ public class MybatisPlusConfig {
      */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+
+        List<ISqlParser> sqlParserList = new ArrayList<>();
+        // 攻击 SQL 阻断解析器、加入解析链
+        sqlParserList.add(new BlockAttackSqlParser());
+        paginationInterceptor.setSqlParserList(sqlParserList);
+        return paginationInterceptor;
     }
 }
