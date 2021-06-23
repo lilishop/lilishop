943d31de9d06aa5652b1c369df7cff41aead492d
lifenlong <pikachu1995@126.com>
2021-06-23 18:06:38 +0800

    显示小程序的客服配置

--- framework/src/main/java/cn/lili/modules/store/mapper/StoreDetailMapper.java
+++ framework/src/main/java/cn/lili/modules/store/mapper/StoreDetailMapper.java
@@ -20,7 +20,7 @@ import java.util.List;
  */
 public interface StoreDetailMapper extends BaseMapper<StoreDetail> {
 
-    @Select("select s.store_logo,s.member_name,s.store_name,s.store_disable,s.self_operated,s.store_address_detail,s.store_address_path,s.store_address_id_path,s.store_center,s.store_desc,s.yzf_sign," +
+    @Select("select s.store_logo,s.member_name,s.store_name,s.store_disable,s.self_operated,s.store_address_detail,s.store_address_path,s.store_address_id_path,s.store_center,s.store_desc,s.yzf_sign,s.yzf_mp_sign," +
             "d.* from li_store s inner join li_store_detail d on s.id=d.store_id where s.id=#{storeId}")
     StoreDetailVO getStoreDetail(String storeId);
 
