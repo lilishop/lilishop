c10b698aed9b84aa46c46e0403364233d1ef3ee1
lifenlong <pikachu1995@126.com>
2021-07-03 13:40:18 +0800

    支持文章传图

--- update-sql/version4.1to4.2.sql
+++ update-sql/version4.1to4.2.sql
@@ -5,3 +5,5 @@ ALTER TABLE li_distribution ADD settlement_bank_branch_name varchar ( 200 );
 
 /** 文章分类添加默认值**/
 ALTER TABLE li_article_category alter column sort set default 0;
+/** 添加分销提现菜单**/
+INSERT INTO `lilishop`.`li_menu`(`id`, `create_by`, `create_time`, `delete_flag`, `update_by`, `update_time`, `description`, `front_route`, `icon`, `level`, `name`, `parent_id`, `path`, `sort_order`, `title`, `front_component`) VALUES (1410862675914764290, 'admin', '2021-07-02 15:27:29', b'0', 'admin', '2021-07-02 15:27:45', 'null', 'distribution/distributionCash', '', 2, 'distributionCash', '1374173575405109248', 'distributionCash', 5.00, '分销提现', 'null');
