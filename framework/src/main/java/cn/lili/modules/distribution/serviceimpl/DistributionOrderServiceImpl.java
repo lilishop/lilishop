acc40dc6fc11908f6fb459526537e3fd10a2be54
lifenlong <pikachu1995@126.com>
2021-07-05 15:45:57 +0800

    店铺配送不展示已经禁用的物流公司

--- framework/src/main/java/cn/lili/modules/distribution/serviceimpl/DistributionOrderServiceImpl.java
+++ framework/src/main/java/cn/lili/modules/distribution/serviceimpl/DistributionOrderServiceImpl.java
@@ -2,6 +2,7 @@ package cn.lili.modules.distribution.serviceimpl;
 
 import cn.hutool.core.date.DateField;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.json.JSONUtil;
 import cn.lili.common.utils.CurrencyUtil;
 import cn.lili.common.utils.PageUtil;
 import cn.lili.modules.distribution.entity.dos.Distribution;
@@ -13,8 +14,12 @@ import cn.lili.modules.distribution.service.DistributionOrderService;
 import cn.lili.modules.distribution.service.DistributionService;
 import cn.lili.modules.order.order.entity.dos.Order;
 import cn.lili.modules.order.order.entity.dos.StoreFlow;
+import cn.lili.modules.order.order.entity.enums.PayStatusEnum;
 import cn.lili.modules.order.order.service.OrderService;
 import cn.lili.modules.order.order.service.StoreFlowService;
+import cn.lili.modules.system.entity.dos.Setting;
+import cn.lili.modules.system.entity.dto.DistributionSetting;
+import cn.lili.modules.system.entity.enums.SettingEnum;
 import cn.lili.modules.system.service.SettingService;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
@@ -56,6 +61,13 @@ public class DistributionOrderServiceImpl extends ServiceImpl<DistributionOrderM
 
     }
 
+    /**
+     * 1.查看订单是否为分销订单
+     * 2.查看店铺流水计算分销总佣金
+     * 3.修改分销员的分销总金额、可提现金额
+     *
+     * @param orderSn 订单编号
+     */
     @Override
     public void payOrder(String orderSn) {
 
@@ -68,11 +80,11 @@ public class DistributionOrderServiceImpl extends ServiceImpl<DistributionOrderM
             List<StoreFlow> storeFlowList = storeFlowService.list(new LambdaQueryWrapper<StoreFlow>()
                     .eq(StoreFlow::getOrderSn, orderSn)
                     .isNotNull(StoreFlow::getDistributionRebate));
-            Double rebate=0.0;
+            Double rebate = 0.0;
             //循环店铺流水记录判断是否包含分销商品
             //包含分销商品则进行记录分销订单、计算分销总额
             for (StoreFlow storeFlow : storeFlowList) {
-                rebate=CurrencyUtil.add(rebate,storeFlow.getDistributionRebate());
+                rebate = CurrencyUtil.add(rebate, storeFlow.getDistributionRebate());
                 DistributionOrder distributionOrder = new DistributionOrder(storeFlow);
                 distributionOrder.setDistributionId(order.getDistributionId());
                 //分销员信息
@@ -80,26 +92,59 @@ public class DistributionOrderServiceImpl extends ServiceImpl<DistributionOrderM
                 distributionOrder.setDistributionName(distribution.getMemberName());
 
                 //设置结算天数(解冻日期)
-//               Setting setting = settingService.get(SettingEnum.DISTRIBUTION_SETTING.name());
-//               DistributionSetting distributionSetting = JSONUtil.toBean(setting.getSettingValue(), DistributionSetting.class);
+                Setting setting = settingService.get(SettingEnum.DISTRIBUTION_SETTING.name());
+                DistributionSetting distributionSetting = JSONUtil.toBean(setting.getSettingValue(), DistributionSetting.class);
                 DateTime dateTime = new DateTime();
                 //默认解冻1天
-//               dateTime.offsetNew(DateField.DAY_OF_MONTH,distributionSetting.getCashDay());
-                dateTime.offsetNew(DateField.DAY_OF_MONTH,1);
+                dateTime.offsetNew(DateField.DAY_OF_MONTH, distributionSetting.getCashDay());
                 distributionOrder.setSettleCycle(dateTime);
                 this.save(distributionOrder);
             }
             //如果包含分销商品则记录会员的分销总额
-            if(rebate!=0.0){
-                distributionService.addRebate(rebate,order.getDistributionId());
+            if (rebate != 0.0) {
+                distributionService.addRebate(rebate, order.getDistributionId());
             }
         }
 
     }
+
+    /**
+     * 1.获取订单判断是否为已付款的分销订单
+     * 2.查看店铺流水记录分销佣金
+     * 3.修改分销员的分销总金额、可提现金额
+     *
+     * @param orderSn 订单编号
+     */
     @Override
     public void cancelOrder(String orderSn) {
+        //根据订单编号获取订单数据
+        Order order = orderService.getBySn(orderSn);
+
+        //判断是否为已付款的分销订单，则获取分销佣金
+        if (order.getDistributionId() != null && order.getPayStatus().equals(PayStatusEnum.PAID.name())) {
+
+            //根据订单编号获取有分销金额的店铺流水记录
+            List<DistributionOrder> distributionOrderList = this.list(new LambdaQueryWrapper<DistributionOrder>()
+                    .eq(DistributionOrder::getOrderSn, orderSn));
+
+            //分销金额
+            Double rebate = 0.0;
+
+            //包含分销商品则进行记录分销订单、计算分销总额
+            for (DistributionOrder distributionOrder : distributionOrderList) {
+                rebate = CurrencyUtil.add(rebate, distributionOrder.getRebate());
+            }
+
+            //如果包含分销商品则记录会员的分销总额
+            if (rebate != 0.0) {
+                distributionService.addRebate(CurrencyUtil.sub(0, rebate), order.getDistributionId());
+            }
+        }
+
+        //修改分销订单的状态
         this.update(new LambdaUpdateWrapper<DistributionOrder>().eq(DistributionOrder::getOrderSn, orderSn)
                 .set(DistributionOrder::getDistributionOrderStatus, DistributionOrderStatusEnum.CANCEL.name()));
+
     }
 
     @Override
@@ -117,18 +162,15 @@ public class DistributionOrderServiceImpl extends ServiceImpl<DistributionOrderM
             //已提交无法重复提交
             //如果未结算则将分销订单取消
             //如果已结算则创建退款分销订单
-            if(distributionOrder.getDistributionOrderStatus().equals(DistributionOrderStatusEnum.CANCEL.name())){
-                return ;
-            } else if(distributionOrder.getDistributionOrderStatus().equals(DistributionOrderStatusEnum.WAIT_BILL.name())){
-                this.update(new LambdaUpdateWrapper<DistributionOrder>().eq(DistributionOrder::getOrderItemSn, storeFlow.getOrderItemSn())
+            if (distributionOrder.getDistributionOrderStatus().equals(DistributionOrderStatusEnum.CANCEL.name())) {
+                return;
+            } else if (distributionOrder.getDistributionOrderStatus().equals(DistributionOrderStatusEnum.WAIT_BILL.name())) {
+                this.update(new LambdaUpdateWrapper<DistributionOrder>()
+                        .eq(DistributionOrder::getOrderItemSn, storeFlow.getOrderItemSn())
                         .set(DistributionOrder::getDistributionOrderStatus, DistributionOrderStatusEnum.CANCEL.name()));
-            }else{
-                //创建分销退款订单
-                DistributionOrder backDistributionOrder = new DistributionOrder();
-
-                this.save(backDistributionOrder);
+            } else {
                 //修改分销员提成金额
-                distributionService.subCanRebate(CurrencyUtil.sub(0,storeFlow.getDistributionRebate()),distributionOrder.getDistributionId());
+                distributionService.subCanRebate(CurrencyUtil.sub(0, storeFlow.getDistributionRebate()), distributionOrder.getDistributionId());
             }
         }
     }
