1ee60c02d3f8f0550d76ff917061e228568f055f
lifenlong <pikachu1995@126.com>
2021-07-23 19:58:32 +0800

    添加引用

--- manager-api/src/main/java/cn/lili/controller/setting/RegionManagerController.java
+++ manager-api/src/main/java/cn/lili/controller/setting/RegionManagerController.java
@@ -2,9 +2,9 @@ package cn.lili.controller.setting;
 
 import cn.lili.common.enums.ResultUtil;
 import cn.lili.common.vo.ResultMessage;
-import cn.lili.modules.system.aspect.DemoSite;
-import cn.lili.modules.system.service.RegionService;
+import cn.lili.modules.system.aspect.annotation.DemoSite;
 import cn.lili.modules.system.entity.dos.Region;
+import cn.lili.modules.system.service.RegionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
--- manager-api/src/main/java/cn/lili/controller/setting/SettingManagerController.java
+++ manager-api/src/main/java/cn/lili/controller/setting/SettingManagerController.java
@@ -2,10 +2,10 @@ package cn.lili.controller.setting;
 
 import cn.hutool.json.JSONUtil;
 import cn.lili.common.enums.ResultCode;
-import cn.lili.common.exception.ServiceException;
 import cn.lili.common.enums.ResultUtil;
+import cn.lili.common.exception.ServiceException;
 import cn.lili.common.vo.ResultMessage;
-import cn.lili.modules.system.aspect.DemoSite;
+import cn.lili.modules.system.aspect.annotation.DemoSite;
 import cn.lili.modules.system.entity.dos.Setting;
 import cn.lili.modules.system.entity.dto.*;
 import cn.lili.modules.system.entity.dto.connect.QQConnectSetting;
