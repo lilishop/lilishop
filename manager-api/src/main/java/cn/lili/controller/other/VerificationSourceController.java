7727c1a45bf8ba91265031aa9b271f4d30f41ece
Chopper <liushuai711@gmail.com>
2021-09-14 16:43:48 +0800

    调整验证码模块，对演示站点做权限处理

--- manager-api/src/main/java/cn/lili/controller/other/VerificationSourceController.java
+++ manager-api/src/main/java/cn/lili/controller/other/VerificationSourceController.java
@@ -4,6 +4,7 @@ import cn.lili.common.enums.ResultUtil;
 import cn.lili.common.vo.PageVO;
 import cn.lili.common.vo.ResultMessage;
 import cn.lili.common.vo.SearchVO;
+import cn.lili.modules.system.aspect.annotation.DemoSite;
 import cn.lili.modules.system.entity.dos.VerificationSource;
 import cn.lili.modules.system.service.VerificationSourceService;
 import cn.lili.mybatis.util.PageUtil;
@@ -51,6 +52,7 @@ public class VerificationSourceController {
 
     @PostMapping
     @ApiOperation(value = "新增验证码资源维护")
+    @DemoSite
     public ResultMessage<VerificationSource> save(VerificationSource verificationSource) {
 
         verificationSourceService.save(verificationSource);
@@ -60,6 +62,7 @@ public class VerificationSourceController {
 
     @PutMapping("/{id}")
     @ApiOperation(value = "更新验证码资源维护")
+    @DemoSite
     public ResultMessage<VerificationSource> update(@PathVariable String id, VerificationSource verificationSource) {
         verificationSource.setId(id);
         verificationSourceService.updateById(verificationSource);
@@ -69,6 +72,7 @@ public class VerificationSourceController {
 
     @DeleteMapping(value = "/{ids}")
     @ApiOperation(value = "删除验证码资源维护")
+    @DemoSite
     public ResultMessage<Object> delAllByIds(@PathVariable List ids) {
 
         verificationSourceService.removeByIds(ids);
