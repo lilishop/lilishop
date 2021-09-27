d432b0a18338a0bbe5be3573d35cc7c124b249ec
Chopper <liushuai711@gmail.com>
2021-09-27 18:13:39 +0800

    swagger ui 优化 无效类去除精简 修改用户上下文获取request/response的方式

--- framework/src/main/java/cn/lili/common/context/ThreadContextHolder.java
+++ framework/src/main/java/cn/lili/common/context/ThreadContextHolder.java
@@ -1,39 +1,29 @@
 package cn.lili.common.context;
 
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * 用户上下文
+ * request / response 获取工具
+ *
  * @author paulG
  * @since 2020/10/16
  **/
 public class ThreadContextHolder {
 
-    private static final ThreadLocal<HttpServletRequest> REQUEST_THREAD_LOCAL_HOLDER = new ThreadLocal<>();
-    private static final ThreadLocal<HttpServletResponse> RESPONSE_THREAD_LOCAL_HOLDER = new ThreadLocal<>();
-
-    public static void remove() {
-        REQUEST_THREAD_LOCAL_HOLDER.remove();
-        RESPONSE_THREAD_LOCAL_HOLDER.remove();
-    }
-
     public static HttpServletResponse getHttpResponse() {
-
-        return RESPONSE_THREAD_LOCAL_HOLDER.get();
-    }
-
-    public static void setHttpResponse(HttpServletResponse response) {
-        RESPONSE_THREAD_LOCAL_HOLDER.set(response);
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        assert servletRequestAttributes != null;
+        return servletRequestAttributes.getResponse();
     }
 
     public static HttpServletRequest getHttpRequest() {
-        return REQUEST_THREAD_LOCAL_HOLDER.get();
-    }
-
-    public static void setHttpRequest(HttpServletRequest request) {
-
-        REQUEST_THREAD_LOCAL_HOLDER.set(request);
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        assert servletRequestAttributes != null;
+        return servletRequestAttributes.getRequest();
     }
 
 
