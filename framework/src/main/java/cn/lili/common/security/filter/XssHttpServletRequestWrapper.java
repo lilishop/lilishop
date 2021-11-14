1d317998af1bad34014a99d97f949e8fbe4c85c8
Chopper <liushuai711@gmail.com>
2021-11-14 15:41:16 +0800

    增加说明

--- framework/src/main/java/cn/lili/common/security/filter/XssHttpServletRequestWrapper.java
+++ framework/src/main/java/cn/lili/common/security/filter/XssHttpServletRequestWrapper.java
@@ -29,7 +29,12 @@ import java.util.Map;
  */
 public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
 
-    private static final String[] ignoreField = {"logo", "url", "photo", "intro", "content", "name", "encrypted", "iv"};
+
+    /**
+     * xss过滤参数
+     * @todo 这里的参数应该更智能些，例如iv，前端的参数包含这两个字母就会放过，这是有问题的
+     */
+    private static final String[] IGNORE_FIELD = {"logo", "url", "photo", "intro", "content", "name", "encrypted", "iv"};
 
     public XssHttpServletRequestWrapper(HttpServletRequest request) {
         super(request);
