1707563118864bd3a28562640dfed4dd216fe48c
Chopper <liushuai711@gmail.com>
2021-11-16 15:27:43 +0800

    VO增加注册时间字段

--- framework/src/main/java/cn/lili/modules/member/entity/vo/MemberVO.java
+++ framework/src/main/java/cn/lili/modules/member/entity/vo/MemberVO.java
@@ -3,9 +3,12 @@ package cn.lili.modules.member.entity.vo;
 import cn.lili.common.enums.ClientTypeEnum;
 import cn.lili.common.security.sensitive.Sensitive;
 import cn.lili.common.security.sensitive.enums.SensitiveStrategy;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
@@ -84,5 +87,9 @@ public class MemberVO implements Serializable {
     @ApiModelProperty(value = "经验值数量")
     private Long experience;
 
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty(value = "创建时间", hidden = true)
+    private Date createTime;
 
 }
