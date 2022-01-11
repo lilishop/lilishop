0460e219f9ca61a3f71c1a3b0c08f288a6be607f
Chopper <liushuai711@gmail.com>
2022-01-11 16:34:18 +0800

    购物车创建时减少一次redis的读写

--- framework/src/main/java/cn/lili/modules/order/cart/render/TradeBuilder.java
+++ framework/src/main/java/cn/lili/modules/order/cart/render/TradeBuilder.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -85,19 +86,17 @@ public class TradeBuilder {
      * 1.构造交易
      * 2.创建交易
      *
-     * @param checkedWay 购物车类型
+     * @param tradeDTO 交易模型
      * @return 交易信息
      */
-    public Trade createTrade(CartTypeEnum checkedWay) {
-        //读取对应购物车的商品信息
-        TradeDTO tradeDTO = cartService.readDTO(checkedWay);
+    public Trade createTrade(TradeDTO tradeDTO) {
 
         //需要对购物车渲染
-        if (isSingle(checkedWay)) {
+        if (isSingle(tradeDTO.getCartTypeEnum())) {
             renderCartBySteps(tradeDTO, RenderStepStatement.singleTradeRender);
-        } else if (checkedWay.equals(CartTypeEnum.PINTUAN)) {
+        } else if (tradeDTO.getCartTypeEnum().equals(CartTypeEnum.PINTUAN)) {
             renderCartBySteps(tradeDTO, RenderStepStatement.pintuanTradeRender);
-        }  else {
+        } else {
             renderCartBySteps(tradeDTO, RenderStepStatement.tradeRender);
         }
 
@@ -128,7 +127,9 @@ public class TradeBuilder {
             for (CartRenderStep render : cartRenderSteps) {
                 try {
                     if (render.step().equals(step)) {
+                        Date date = new Date();
                         render.render(tradeDTO);
+                        log.error(render.getClass().getName() + "-" + (System.currentTimeMillis() - date.getTime()));
                     }
                 } catch (ServiceException e) {
                     throw e;
