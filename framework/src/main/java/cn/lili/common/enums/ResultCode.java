096ab34ae83f29e2339bb3c763976d4f0c95777b
paulGao <paulgao0831@gmail.com>
2022-11-22 15:29:22 +0800

    fix: 优化文件上传，删除冗余文件

--- framework/src/main/java/cn/lili/common/enums/ResultCode.java
+++ framework/src/main/java/cn/lili/common/enums/ResultCode.java
@@ -37,6 +37,7 @@ public enum ResultCode {
     LIMIT_ERROR(1003, "访问过于频繁，请稍后再试"),
     ILLEGAL_REQUEST_ERROR(1004, "非法请求，请重新刷新页面操作"),
     IMAGE_FILE_EXT_ERROR(1005, "不支持图片格式"),
+    FILE_NOT_EXIST_ERROR(1011, "上传文件不能为空"),
     FILE_TYPE_NOT_SUPPORT(1010, "不支持上传的文件类型！"),
     PLATFORM_NOT_SUPPORTED_IM(1006, "平台未开启IM"),
     STORE_NOT_SUPPORTED_IM(1007, "店铺未开启IM"),
