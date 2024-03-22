42ea2dc986a94228b2e8f2ef77480f418ec8ad79
chc <1501738723@qq.com>
2024-03-22 19:07:02 +0800

    订单后台收款后返回状态不对

--- framework/src/main/java/cn/lili/modules/payment/kit/params/impl/TradeCashier.java
+++ framework/src/main/java/cn/lili/modules/payment/kit/params/impl/TradeCashier.java
@@ -73,7 +73,7 @@ public class TradeCashier implements CashierExecute {
             for (Order order : orders) {
                 //如果订单已支付，则不能发器支付
                 if (order.getPayStatus().equals(PayStatusEnum.PAID.name())) {
-                    throw new ServiceException(ResultCode.PAY_PARTIAL_ERROR);
+                    throw new ServiceException(ResultCode.PAY_DOUBLE_ERROR);
                 }
                 //如果订单状态不是待付款，则抛出异常
                 if (!order.getOrderStatus().equals(OrderStatusEnum.UNPAID.name())) {
