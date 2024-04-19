2d052ffb1a055a1c69065772bbfc33048470249a
铉清 <13698617+xuanqings@user.noreply.gitee.com>
2024-04-19 05:17:44 +0000

    多了一个结束符号进行移除
    Signed-off-by: 铉清 <13698617+xuanqings@user.noreply.gitee.com>

--- im-api/src/main/java/cn/lili/controller/store/seat/SeatSettingStoreController.java
+++ im-api/src/main/java/cn/lili/controller/store/seat/SeatSettingStoreController.java
@@ -4,7 +4,7 @@ import cn.lili.common.enums.ResultUtil;
 import cn.lili.common.security.context.UserContext;
 import cn.lili.common.vo.ResultMessage;
 import cn.lili.modules.im.entity.dos.SeatSetting;
-import cn.lili.modules.im.service.SeatSettingService;;
+import cn.lili.modules.im.service.SeatSettingService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
